07914de6a2073d41ec1464f58a0ba22b67895bfe
Mo <moho2010@live.ie>
2025-04-03 13:01:42 -0400

    more Annotations work

--- src/main/java/com/mostlyactive/store/OrderService.java
+++ src/main/java/com/mostlyactive/store/OrderService.java
@@ -1,6 +1,8 @@
 package com.mostlyactive.store;
 
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
@@ -11,7 +13,20 @@ public class OrderService {
 
     public OrderService(PaymentService paymentService){
         this.paymentService = paymentService;
+        System.out.println("OrderService created");
     }
+
+    //postconstruct annotation runs after the constructure. instance made now do this.
+    @PostConstruct
+    public void init(){
+        System.out.println("orderService PostConstruct");
+    }
+
+    @PreDestroy
+    public void cleanup(){
+        System.out.println("orderService destroyed");
+    }
+
     public void placeOrder(){
         paymentService.processPayment(10);
     }
