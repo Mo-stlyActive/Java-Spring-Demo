0470e70a830c3e8aa587e12fc1567db9bf84838c
Mo <moho2010@live.ie>
2025-04-04 11:05:34 -0400

    mySQL setup

--- src/main/java/com/mostlyactive/store/StoreApplication.java
+++ src/main/java/com/mostlyactive/store/StoreApplication.java
@@ -3,18 +3,13 @@ package com.mostlyactive.store;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class StoreApplication {
 
 	public static void main(String[] args) {
 
-		ConfigurableApplicationContext context = SpringApplication.run(StoreApplication.class, args);
-		var orderService = context.getBean(OrderService.class);
-		orderService.placeOrder();
-
-		context.close();
+		ApplicationContext context = SpringApplication.run(StoreApplication.class, args);
 
 	}
 
